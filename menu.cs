649635d9526732956632fb876ec5e5b77fb4cf90
mef25 <148797367+mef25@users.noreply.github.com>
2023-11-06 14:22:29 +0300

    Butonlar için script

---
+++ menu.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class menu : MonoBehaviour
+{
+    
+    public void menuu()
+    {
+       SceneManager.LoadScene(0);
+       
+    }
+
+    public void selectlevel()
+    {
+        SceneManager.LoadScene(11);
+    }
+  
+ 
+
+
+
+}
