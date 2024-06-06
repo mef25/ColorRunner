49260b1c6ad44b8dde2159202beabfd014ea1006
mef25 <148797367+mef25@users.noreply.github.com>
2024-06-06 13:13:28 +0300

    Update renkDegisme.cs

--- renkDegisme.cs
+++ renkDegisme.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
 
-
 public class renkDegisme : randomYazi
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "sagduvar" || other.gameObject.tag == "solduvar")
         {
-            GetComponent<SkinnedMeshRenderer>().material.color = other.GetComponent<MeshRenderer>().material.color;
-            
+            GetComponent<SkinnedMeshRenderer>().material.color = other.GetComponent<MeshRenderer>().material.color; 
         }  
     }
-
 }
