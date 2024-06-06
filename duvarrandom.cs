c2d51eeacade7b95e07d2fba2c4ede7132b7569c
mef25 <148797367+mef25@users.noreply.github.com>
2024-06-06 13:10:40 +0300

    Update duvarrandom.cs

--- duvarrandom.cs
+++ duvarrandom.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class duvarrandom : randomYazi
 {
     public GameObject gameObject;
     
-   
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
