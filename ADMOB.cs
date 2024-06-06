3485a98d1cbdfe3b215b0801bbb204ec93d388ca
mef25 <148797367+mef25@users.noreply.github.com>
2024-06-06 13:08:03 +0300

    Update ADMOB.cs

--- ADMOB.cs
+++ ADMOB.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using GoogleMobileAds.Api;
 using System;
@@ -8,7 +6,6 @@ using Firebase;
 
 public class ADMOB : MonoBehaviour
 {
-
     private FirebaseApp app;
 
     public void Start()
@@ -18,17 +15,12 @@ public class ADMOB : MonoBehaviour
             var dependencyStatus = task.Result;
             if (dependencyStatus == Firebase.DependencyStatus.Available)
             {
-                // Create and hold a reference to your FirebaseApp,
-                // where app is a Firebase.FirebaseApp property of your application class.
                 app = Firebase.FirebaseApp.DefaultInstance;
-
-                // Set a flag here to indicate whether Firebase is ready to use by your app.
             }
             else
             {
                 UnityEngine.Debug.LogError(System.String.Format(
                   "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-                // Firebase Unity SDK is not safe to use here.
             }
         });
     }
